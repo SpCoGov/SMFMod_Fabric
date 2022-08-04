355b3d9105e7f11d8fef843caedef9be6cec598b
SpCoGov <haimiancouna@gmail.com>
2022-08-04 22:00:13 +0800

    不知道

--- src/main/java/com/spco/spco/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/com/spco/spco/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,20 +1,3 @@
-/*
- * Copyright (c) 2020 C4
- *
- * This file is part of Vein Mining, a mod made for Minecraft.
- *
- * Vein Mining is free software: you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation, either version 3 of
- * the License, or any later version.
- *
- * Vein Mining is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with Vein Mining.
- * If not, see <https://www.gnu.org/licenses/>.
- */
-
 package com.spco.spco.mixin;
 
 import com.spco.spco.enchantment.veinmining.logic.VeinMiningLogic;
