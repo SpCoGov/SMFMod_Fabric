d03e1f2b2f01e98c804b651ec649a3d79fce60fe
SpCoGov <haimiancouna@gmail.com>
2022-08-04 21:49:15 +0800

    修改图标

--- src/main/java/com/spco/spco/mixin/MinecraftTitleMixin.java
+++ src/main/java/com/spco/spco/mixin/MinecraftTitleMixin.java
@@ -2,10 +2,14 @@ package com.spco.spco.mixin;
 
 import com.spco.spco.SpCo;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.io.InputStream;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftTitleMixin {
@@ -21,4 +25,20 @@ public abstract class MinecraftTitleMixin {
 
         cir.setReturnValue(titleBuilder);
     }
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;setIcon(Ljava/io/InputStream;Ljava/io/InputStream;)V"))
+    private void setupIcon(Window instance, InputStream icon16, InputStream icon32) {
+
+        final InputStream stream16 = SpCo.class.getResourceAsStream("/assets/smf/icon_16x16.png");
+        final InputStream stream32 = SpCo.class.getResourceAsStream("/assets/smf/icon_32x32.png");
+
+        if (stream16 == null || stream32 == null) {
+            instance.setIcon(icon16, icon32);
+            return;
+        }
+
+        instance.setIcon(stream16, stream32);
+    }
+
+
 }
